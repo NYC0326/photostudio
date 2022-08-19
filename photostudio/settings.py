ac10dd4c44fc35f22f40fe4671fd7c6c32395d67
NYC0326 <71819307+NYC0326@users.noreply.github.com>
2022-08-20 00:03:49 +0900

    Update settings.py

--- photostudio/settings.py
+++ photostudio/settings.py
@@ -14,7 +14,8 @@ SECRET_KEY = 'django-insecure-4pzrjbnygbponzqweq+y1+zs!siyza7w#qn%g&@aa@rl!+^qip
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".cloudtype.app"]
+CSRF_TRUSTED_ORIGINS = ['https://*.cloudtype.app']
 
 
 # Application definition
